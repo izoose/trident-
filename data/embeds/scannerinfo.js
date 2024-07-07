2876646b65f1654ca3312338ca93e54f38d7d257
zoose <124001738+izoose@users.noreply.github.com>
2024-07-07 12:05:56 -0700

    Create scannerinfo.js

---
+++ data/embeds/scannerinfo.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "title": "Scanner Information - #ChannelName",
+    "description": "**Number of Creators:** 69",
+    "color": null,
+    "fields": [
+      {
+        "name": "Blacklisted Asset Types:",
+        "value": "Decals,\nImages,\nLua,\nLocalizationTableTranslation"
+      }
+    ],
+    "footer": {
+      "text": "- Trident",
+      "icon_url": "https://cdn.discordapp.com/icons/1171977315799011449/6a2416b6207dfadb181c03645eff933f.webp"
+    }
+  }
